c30865fdd38ba56168a98c38cb21b550064a8831
raul <raul.argemi.7e6@itb.cat>
2024-04-25 16:03:48 +0200

    Cambios tangibles

--- src/router/club.router.js
+++ src/router/club.router.js
@@ -1,31 +1,41 @@
-const router = require("express").Router();
-const Club = require("../model/club.model");
-const Company = require("../model/company.model");
+const express = require('express');
+const multer = require('multer');
+const Club = require('../model/club.model');
+const Company = require('../model/company.model');
 
-router.post("/club", async (req, res) => {
+const router = express.Router();
+
+const upload = multer({ dest: 'uploads/' }); 
+
+router.post('/club', upload.single('club_img'), async (req, res) => {
   try {
     const { company_nif, club_name, club_rules, club_description, club_schedule } = req.body;
+    const club_img = req.file.path; 
+
     // Verifica si la compañía existe
     const company = await Company.findOne({ where: { company_nif } });
     if (!company) {
-      return res.status(404).json({ error: "La compañía no existe" });
+      return res.status(404).json({ error: 'La compañía no existe' });
     }
+
     // Crea el club asociado a la compañía
     const club = await Club.create({
       club_owner: company_nif,
       club_name,
       club_rules,
       club_description,
-      club_schedule
+      club_schedule,
+      club_img
     });
+
     return res.status(201).json(club);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: "Error interno del servidor" });
+    return res.status(500).json({ error: 'Error interno del servidor' });
   }
 });
 
-router.post("/clubs", async (req, res) => {
+router.post('/clubs', async (req, res) => {
   const clubData = req.body;
   await Club.sync();
   const updateClub = await Club.create({
@@ -38,7 +48,7 @@ router.post("/clubs", async (req, res) => {
   res.status(201).json({
     ok: true,
     status: 201,
-    message: "Created Club"
+    message: 'Created Client'
   });
 });
 
@@ -64,5 +74,34 @@ router.post("/company/nif", async (req, res) => {
   }
 });
 
+router.put('/club/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { company_nif, club_name, club_rules, club_description, club_schedule, club_img } = req.body;
+    const club = await Club.findOne({ where: { id: id } });
+
+    if (!club) {
+      return res.status(404).json({ error: 'Club not found' });
+    }
+
+    club.company_nif = company_nif;
+    club.club_name = club_name;
+    club.club_rules = club_rules;
+    club.club_description = club_description;
+    club.club_schedule = club_schedule;
+    club.club_img = club_img;
+
+    await club.save();
+
+    res.status(200).json({
+      ok: true,
+      status: 200,
+      message: 'Club updated successfully'
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 module.exports = router;
