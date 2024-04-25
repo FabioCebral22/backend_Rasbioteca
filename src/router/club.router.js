7300732e9029f584ddf838fdac4c2b074ced7c3e
Fabio <fabio.cebral.7e5@itb.cat>
2024-04-25 17:07:56 +0200

    Comentario

--- src/router/club.router.js
+++ src/router/club.router.js
@@ -5,12 +5,10 @@ const Company = require("../model/company.model");
 router.post("/club", async (req, res) => {
   try {
     const { company_nif, club_name, club_rules, club_description, club_schedule } = req.body;
-    // Verifica si la compañía existe
     const company = await Company.findOne({ where: { company_nif } });
     if (!company) {
       return res.status(404).json({ error: "La compañía no existe" });
     }
-    // Crea el club asociado a la compañía
     const club = await Club.create({
       club_owner: company_nif,
       club_name,
