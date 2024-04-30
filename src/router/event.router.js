99185ba4826fcfa8b23e41d05921ddf1b1c74cef
Fabio <fabio.cebral.7e5@itb.cat>
2024-04-30 16:36:23 +0200

    Añadir,Eliminar,Ver EVENTOS

--- src/router/event.router.js
+++ src/router/event.router.js
@@ -23,15 +23,21 @@ router.post('/events', upload.single('event_img'), async (req, res) => {
     });
   });
   
-router.get("/events", async (req, res) => {
-  try {
-    const events = await Event.find();
-    res.status(200).json(events);
-  } catch (error) {
-    console.error("Error al obtener los eventos:", error);
-    res.status(500).send("Hubo un error al obtener los eventos. Por favor, inténtalo de nuevo más tarde.");
-  }
+  router.get("/findevents", async (req, res) => {
+    try {
+        const events = await Event.findAll();
+
+        res.status(200).json({
+            ok: true,
+            status: 200,
+            body: events
+        });
+    } catch (error) {
+        console.error('Error al buscar eventos:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
+
 
 router.post("/events", async (req, res) => {
   try {
@@ -43,6 +49,26 @@ router.post("/events", async (req, res) => {
     res.status(500).send("Hubo un error al crear el evento. Por favor, inténtalo de nuevo más tarde.");
   }
 });
+router.put("/event/delete", async (req, res) => {
+  const eventId = req.body.event_id;
+  try {
+      const deletedEvent = await Event.destroy({
+          where: {
+              event_id: eventId
+          }
+      });
+
+      if (deletedEvent === 0) {
+          return res.status(404).send("No se encontró ningún evento con el ID proporcionado.");
+      }
+
+      res.send(`Se ha eliminado el evento con el ID: ${eventId}`);
+  } catch (error) {
+      console.error("Error al eliminar el evento:", error);
+      res.status(500).send("Hubo un error al eliminar el evento. Por favor, inténtalo de nuevo más tarde.");
+  }
+});
+
 
 
 module.exports = router;
