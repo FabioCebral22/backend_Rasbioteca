30e4d729ff6ec3761754b673e21ce1f306cbecbf
Fabio <fabio.cebral.7e5@itb.cat>
2024-05-14 18:39:54 +0200

    reviews

--- src/router/review.router.js
+++ src/router/review.router.js
@@ -7,12 +7,18 @@ const Reviews =require("../model/reviews.model")
 
 router.post("/reviews", async (req, res) => {
     const dataReview = req.body;
+    const ticket = await Ticket.findOne({ 
+        where: { ticket_id: dataReview.ticket_id }
+      });
+    const event = await Event.findOne({
+        where: { event_id: ticket.event_id }
+      });
 
     const newReview = await Reviews.create({
         review_data: dataReview.review_data,
         review_value: dataReview.review_value,
         client_id: dataReview.client_id,
-        club_id: dataReview.club_id,
+        club_id: event.club_id,
         createdAt: new Date(),
         updatedAt: new Date()
     });
@@ -24,6 +30,45 @@ router.post("/reviews", async (req, res) => {
     });
 });
 
-module.exports = router;
+router.post('/client-reviews', async (req, res) => {
+    try {
+      const { client_email } = req.body;
+  
+      const client = await Client.findOne({ 
+        where: { client_email: client_email }
+      });
 
+      const reviews = await Reviews.findAll({ 
+        where: { client_id: client.client_id }
+      });
+  
+      res.status(200).json({
+        ok: true,
+        status: 200,
+        reviews: reviews
+      });
+    } catch (error) {
+      console.error('Error al obtener las reseñas del cliente:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
+  
+  router.post('/club-reviews', async (req, res) => {
+    try {
+      const { club_id } = req.body;
+  
+      const reviews = await Reviews.findAll({
+        where: { club_id: club_id }
+      });
+  
+      res.status(200).json({
+        ok: true,
+        status: 200,
+        reviews: reviews
+      });
+    } catch (error) {
+      console.error('Error al obtener las reseñas del club:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
 module.exports = router;
